76fd73fc36a6879a1f4cfce089c292bd711fe174
Frank <frank@email.com>
2025-02-26 00:31:41 -0300

    CREATING MANU INTERATIONS CREATING ABC CLASS CREATING FACTORY LEVEL 1 BG IMPLEMENTATION: PARALLAX

--- code/Entity.py
+++ code/Entity.py
@@ -1,10 +1,13 @@
 from abc import ABC, abstractmethod
 
 import pygame
+from pygame import Surface
 
 
 class Entity(ABC):
-    def __init__(self, name: str, position: tuple[]):
+    surf: Surface
+
+    def __init__(self, name: str, position: tuple):
         self.name = name
         self.surf = pygame.image.load('./asset/' + name + '.png')
         self.surf = self.surf.get_rect(left=position[0], top=position[1])
